4c4d4138087460534fbfb6d21e716e96b4a79ef1
JonathanGithumbi <jonathan.m.githumbi@gmail.com>
2022-05-27 08:53:55 +0300

    moved folders

--- user_account/admin.py
+++ user_account/admin.py
@@ -1,6 +1,6 @@
-#from django.contrib import admin
-#from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-#from user_account.models import CustomUser
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
+from user_account.models import CustomUser
 ##from administrator.models import Administrator
 ##
 ##
@@ -12,5 +12,4 @@
 ##class UserAdmin(BaseUserAdmin):
 ##    inlines = (AdministratorInline,)
 ##
-##
-##admin.site.register(CustomUser,UserAdmin)
+admin.site.register(CustomUser,UserAdmin)
